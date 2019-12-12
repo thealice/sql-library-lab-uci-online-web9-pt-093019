b5aaa6b0de723fcbdbe48539586c5f32d0a77bc3
Alice Freda <alicefreda@gmail.com>
2019-12-12 02:39:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,20 +8,33 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1;"
+  "SELECT species, COUNT(species) 
+  FROM characters 
+  GROUP BY species 
+  ORDER BY COUNT(species) 
+  DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors
-    INNER JOIN series ON authors.id = series.author_id
-    JOIN subgenres ON series.subgenre_id = subgenres.id;"
+  "SELECT authors.name, subgenres.name 
+    FROM authors
+    INNER JOIN series 
+    ON authors.id = series.author_id
+    JOIN subgenres 
+    ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series
-    INNER JOIN authors ON series.author_id = authors.id
-    JOIN characters ON authors.id = characters.author_id
-    WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(characters.species) DESC LIMIT 1;"
+  "SELECT series.title 
+    FROM series
+    INNER JOIN authors 
+    ON series.author_id = authors.id
+    JOIN characters 
+    ON authors.id = characters.author_id
+    WHERE characters.species = 'human' 
+    GROUP BY series.title 
+    ORDER BY COUNT(characters.species) DESC 
+    LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
